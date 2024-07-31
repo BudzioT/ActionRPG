f8f04fc01d405aaabb341eb973fe506eaae0f1b4
BudzioT <Budzio.zsm1@gmail.com>
2024-07-31 18:49:34 +0200

    Added input, player's movement and started collisions

---
+++ Scenes/Enitties/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+"""----------------------- GLOBAL VARIABLES -----------------------"""
+# Movement variables
+@export_category("Movement")
+@export var speed: float = 100.0
+@export var deceleration: float = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	"""Process physics changes"""
+	# Get the direction
+	var direction = Vector2(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	).normalized()
+	
+	# If there is movement, update the velocity
+	if direction.length() > 0:
+		velocity = direction * speed
+	# Otherwise deccelerate slowly
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+		
+	move_and_slide()
